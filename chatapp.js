93afcafc31b45409d09d2a6baea01772e8dbcaf4
linh-dinh <linh.dinh@overdose.digital>
2021-12-03 15:21:18 +0700

    launch

---
+++ chatapp.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require('cors')
+const http = require('http');
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const socket_io = require('socket.io');
+require('dotenv').config();
+
+const initWebRoutes = require('./routes/web');
+
+let app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+let server = http.createServer(app);
+
+app.use("/", initWebRoutes());
+
+let port = process.env.PORT || 3000;
+
+server.listen(port, ()=>{
+    console.log(`app is running at port: http://localhost:${port}`);
+})
